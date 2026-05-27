67f3c47ab094cd4e7d82909a5e95c036262b2efa
Dreamcaster1 <maslovskyy.fb@gmail.com>
2026-05-27 12:22:36 +0100

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Max29052007.",
-    database: process.env.DB_NAME || "reactexpress",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 10,
     queueLimit: 0,
